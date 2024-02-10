811b3069400427d498d269bbd23674477c29c448
jaiken17 <jaiken17@ubishops.ca>
2024-02-10 15:36:00 -0500

    offloaded precision definitions to own module

---
+++ src/df_precision.f90
@@ -0,0 +1,8 @@
+module df_precision
+    use,intrinsic :: iso_fortran_env,only: r64=>real64, i32=>int32
+
+    ! Define kind parameters here
+    integer,parameter :: rk = r64,      &
+                         ik = i32
+
+end module df_precision
