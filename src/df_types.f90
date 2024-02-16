db4d4ed3ef487af2459777117443a36114e6f1f3
jaiken17 <jaiken17@ubishops.ca>
2024-02-16 17:32:25 -0500

    moved type enum to own module

---
+++ src/df_types.f90
@@ -0,0 +1,10 @@
+module df_types
+    implicit none
+
+    integer,parameter,public :: INTEGER = 1,       &
+                                REAL = 2,          &
+                                LOGICAL = 3,       &
+                                CHARACTER = 4,     &
+                                COMPLEX = 5
+
+end module df_types
