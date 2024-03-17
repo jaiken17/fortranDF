a178db1a3ba8734c68535d3bb0e5a0e58eedea10
jaiken17 <jaiken17@ubishops.ca>
2024-03-16 20:26:37 -0400

    swap INTEGER_NUM and REAL_NUM vals

--- src/df_types.f90
+++ src/df_types.f90
@@ -1,8 +1,8 @@
 module df_types
     implicit none
 
-    integer,parameter,public :: INTEGER_NUM = 1,       &
-                                REAL_NUM = 2,          &
+    integer,parameter,public :: REAL_NUM = 1,       &
+                                INTEGER_NUM = 2,          &
                                 LOGICAL_NUM = 3,       &
                                 CHARACTER_NUM = 4,     &
                                 COMPLEX_NUM = 5
